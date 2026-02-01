ae569fabe1d5f482c9cd4a29817b2969c40cd21c
Logan Pfeiffer <Lpfeiffer48@usf.edu>
2026-02-01 18:59:31 -0500

    Added inventory Metrics Calculations, and add one variable I missed

--- Inventory_Reorder_Assistant.js
+++ Inventory_Reorder_Assistant.js
@@ -2,11 +2,19 @@ console.log("Hello, World!");
 
 // Variables 
 let itemName = "Banana";
-let currentStock = 348;
-let reorderLevel = 250;
-let targetStock = 450;
-let weeklyDemand = 175;
-let leadTimeWeeks = 2;
+let unitCost = 1.35; // Cost per LB 
+let currentStock = 348; // IN LBs
+let reorderLevel = 250; // IN LBs
+let targetStock = 450; // IN LBs
+let weeklyDemand = 175; // IN LBs
+let leadTimeWeeks = 2; // Supplier time taken to replenish stock (in weeks)
 
+// inventory Metrics Calculations 
 
+let weeksOfCover = weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity;
+let stockDeficit = Math.max(0, targetStock - currentStock);
+let reorderQuantity = (currentStock <= reorderLevel || weeksOfCover < leadTimeWeeks) ? Math.ceil(stockDeficit) : 0 ;
+let estimatedReorderCost = reorderQuantity * unitCost;
+let reorderNow = currentStock <= reorderLevel || weeksOfCover < leadTimeWeeks;
 
+// Display to Console 
