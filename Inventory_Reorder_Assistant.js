a9762b0ad45d0f19de5e26ad5ab999fcffdb2d5c
Logan Pfeiffer <Lpfeiffer48@usf.edu>
2026-02-01 18:48:54 -0500

    Variables

--- Inventory_Reorder_Assistant.js
+++ Inventory_Reorder_Assistant.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!");
+
+// Variables 
+let itemName = "Banana";
+let currentStock = 348;
+let reorderLevel = 250;
+let targetStock = 450;
+let weeklyDemand = 175;
+let leadTimeWeeks = 2;
+
+
+
